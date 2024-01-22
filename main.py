84c6709f1c1d03ce3ce898682425c2a700beab56
KarienyeRobert <karienyerobert@gmail.com>
2024-01-22 20:56:27 +0300

    commit

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 class User():
     def __init__( self, name, age, gender):
-        self.name=name
+        self.name = name
         self.age = age
-        self.gender= gender
+        self.gender = gender
+        
+
+    def show_details(self) :
+     print("Personal Details")
+     print("")
+     print
